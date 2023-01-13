ff9a3e0bb9e4efcfc20182280fb4ac28f450a801
Taewon Kim <peterkim426@outlook.com>
2023-01-13 01:31:12 -0800

    v1.0.2 update

---
+++ main.py
@@ -0,0 +1,41 @@
+import argparse
+import json
+import os
+from zipfile import ZipFile
+
+# Parse command line arguments to determine which action to take
+parser = argparse.ArgumentParser()
+parser.add_argument("-b", "--build", help="Build the mod", action="store_true")
+args = parser.parse_args()
+
+if args.build:
+    # Create build directory if it doesn't exist
+    if not os.path.exists("build"):
+        os.makedirs("build")
+
+    # Read the manifest.json
+    mod_unique_id = ""
+    mod_version = ""
+    with open("src/manifest.json", "r") as manifest_file:
+        manifest = json.load(manifest_file)
+        mod_unique_id = manifest["UniqueID"]
+        mod_version = manifest["Version"]
+
+    # Create zip file
+    with ZipFile(os.path.join("build", f"baechus-portraits-cp-v{mod_version}.zip"), 'w') as zip_obj:
+        # Iterate over all the files in directory
+        for root, dirs, files in os.walk("src"):
+            for filename in files:
+                infilePath = os.path.join(root, filename)
+                outfilePath = os.path.join(mod_unique_id, filename)
+
+                # Add file to zip
+                zip_obj.write(infilePath, outfilePath)
+
+        for root, dirs, files in os.walk("assets"):
+            for filename in files:
+                infilePath = os.path.join(root, filename)
+                outfilePath = os.path.join(mod_unique_id, root, filename)
+
+                # Add file to zip
+                zip_obj.write(infilePath, outfilePath)
